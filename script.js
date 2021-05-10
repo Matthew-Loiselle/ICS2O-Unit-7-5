b0870ca2018815119ed3cc3d0ac54c3b3b352e0e
MatthewLoiselle <>
2021-05-10 13:49:32 +0000

    style

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ let secondNum = 0
 let counter = 0
 let answer = 0
 
-function calculate () {
-  firstNum = document.getElementById('first-number').value
-  secondNum = document.getElementById('second-number').value
+function calculate() {
+  firstNum = document.getElementById('firstnumber').value
+  secondNum = document.getElementById('secondnumber').value
   firstNum = parseInt(firstNum)
   secondNum = parseInt(secondNum)
- 
+
   if (firstNum >= 0) {
-  for (counter = 0; counter < firstNum; counter++) {
-    answer = answer + secondNum
+    for (counter = 0; counter < firstNum; counter++) {
+      answer = answer + secondNum
+    }
+    document.getElementById('answer').innerHTML = (answer)
+  } else {
+    for (counter = 0; counter > firstNum; counter--) {
+      answer = answer - secondNum
+    }
+    document.getElementById('answer').innerHTML = (answer)
   }
-  document.getElementById('answer').innerHTML = (answer)
- } else {
-  for (counter = 0; counter > firstNum; counter--) {
-    answer = answer - secondNum
-  }
-  document.getElementById('answer').innerHTML = (answer)
- }
- answer = 0
+  answer = 0
 }
